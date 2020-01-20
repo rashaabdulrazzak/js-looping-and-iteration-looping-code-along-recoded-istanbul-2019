fbbc2900aa1d6ecac2a38478ccefcdfa14879e42
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-01-20 18:25:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+let wrapgifts =["Ada", "Brendan", "Ali"];
+let str = birthday;
+function writeCards(wrapgifts, str)
+{
+  let  msgfift=[];
+  for(let i=0;i < wrapgifts.length;i++){
+    msgfift.push(`Thank you, ${wrapgifts[i]}, for the wonderful birthday gift!"`);
+    console.log(`Thank you, ${wrapgifts[i]}, for the wonderful birthday gift!"`);
+  }
+  return msgfift;
+}
+
+function countDown(num){
+    while (num >=0){
+      console.log(`${num}`)
+      num--;
+    }
+}
+countDown(10);
