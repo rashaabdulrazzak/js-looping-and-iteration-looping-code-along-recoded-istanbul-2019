1fd2f0fd9cc18ea91bd84aaa6fdac6cf610d6a88
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-01-20 18:58:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+let wrapgifts =["Ada", "Brendan", "Ali"];
+let str = 'birthday';
+let srtingr = '';
+function writeCards(wrapgifts, str)
+{
+  let  msgfift=[];
+  for(let i=0;i < wrapgifts.length;i++){
+    srtingr= 'Thank you, '+wrapgifts[i]+", for the wonderful "+ `${str}` + " gift!";
+    msgfift.push(`${srtingr}`);  
+    console.log(`${srtingr}`);
+  }
+  return msgfift;
+}
+myresult =  writeCards(wrapgifts, str);
+
+
+function countDown(num){
+    while (num >=0){
+      console.log(num);
+      num--;
+    }
+}
+countDown(10);
